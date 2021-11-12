b62d93fa68101552f9fe80f8880dd7e239fce931
Romaric Zounlome <rzounlom@gmail.com>
2021-11-12 09:26:23 -0500

    complete ui styles, layout, basic functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,24 @@
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
+  const listEvents = events.map((evt) => <EventItem key={evt.id} evt={evt} />);
   return (
-    <Layout title="Events">
-      <h1>My Events</h1>
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {listEvents}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  const listEvents = events.map((evt) => <EventItem key={evt.id} evt={evt} />);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1> Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {listEvents}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">Vew All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  };
 }
