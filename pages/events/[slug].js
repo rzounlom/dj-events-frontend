d51d4b97d448ca50d8289521aa76e3d8fb493c72
Romaric Zounlome <rzounlom@gmail.com>
2021-11-11 20:49:13 -0500

    create Layout, Hader, and Footer components

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,13 @@
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+
 export default function EventPage() {
+  const router = useRouter();
+
+  console.log(router);
   return (
-    <div>
+    <Layout title="Event">
       <h1>My Event</h1>
-    </div>
+    </Layout>
   );
 }
